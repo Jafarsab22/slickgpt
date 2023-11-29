bdc0309073360c839394d7662c307715d77da150
Jafarsab22 <66467042+Jafarsab22@users.noreply.github.com>
2023-11-29 09:08:15 +0000

    Update Chat.svelte

--- src/lib/Chat.svelte
+++ src/lib/Chat.svelte
@@ -1,76 +1,79 @@
 <script lang="ts">
-	import { afterUpdate, beforeUpdate, onMount } from 'svelte';
-	import { ProgressRadial } from '@skeletonlabs/skeleton';
-	import snarkdown from 'snarkdown';
-	import { afterNavigate } from '$app/navigation';
-	import type { Chat } from '$misc/shared';
-	import { chatStore, enhancedLiveAnswerStore, isLoadingAnswerStore } from '$misc/stores';
-	import ChatMessages from './ChatMessages.svelte';
+    import { onMount, onDestroy } from 'svelte';
+    import { afterUpdate, beforeUpdate, onMount } from 'svelte';
+    import { ProgressRadial } from '@skeletonlabs/skeleton';
+    import snarkdown from 'snarkdown';
+    import { afterNavigate } from '$app/navigation';
+    import type { Chat } from '$misc/shared';
+    import { chatStore, enhancedLiveAnswerStore, isLoadingAnswerStore } from '$misc/stores';
+    import ChatMessages from './ChatMessages.svelte';
 
-	export let slug: string;
-	export let chat: Chat | undefined = undefined;
+    export let slug: string;
+    export let chat: Chat | undefined = undefined;
 
-	$: if ($chatStore[slug]) {
-		// If this is used in the "Shared chat" view, the chat is not in the local store.
-		// Instead it's loaded from the db and passed in as a prop.
-		chat = $chatStore[slug];
-	}
+    $: if ($chatStore[slug]) {
+        chat = $chatStore[slug];
+    }
 
-	// Autoscroll: https://svelte.dev/tutorial/update
-	let div: HTMLElement | null | undefined;
-	let autoscroll: boolean | null | undefined;
+    let div: HTMLElement | null | undefined;
+    let autoscroll: boolean | null | undefined;
+    let timer: ReturnType<typeof setTimeout>;
+    let isTimeUp = false;
 
-	onMount(() => {
-		// bind to the *scrollable* element by it's id
-		// note: element is not exposed in this file, it lives in app.html
-		div = document.getElementById('page');
-	});
+    onMount(() => {
+        div = document.getElementById('page');
 
-	beforeUpdate(() => {
-		autoscroll = div && div.offsetHeight + div.scrollTop > div.scrollHeight - 20;
-	});
+        timer = setTimeout(() => {
+            isTimeUp = true;
+            console.log("Time is up!");
+        }, 1 * 60 * 1000); // 30 minutes in milliseconds
+    });
 
-	afterUpdate(() => {
-		if (autoscroll) div?.scrollTo({ top: div.scrollHeight, behavior: 'smooth' });
-	});
+    onDestroy(() => {
+        clearTimeout(timer);
+    });
 
-	// autoscroll to bottom after navigation
-	afterNavigate(() => {
-		div?.scrollTo({ top: div.scrollHeight, behavior: 'smooth' });
-	});
+    beforeUpdate(() => {
+        autoscroll = div && div.offsetHeight + div.scrollTop > div.scrollHeight - 20;
+    });
+
+    afterUpdate(() => {
+        if (autoscroll) div?.scrollTo({ top: div.scrollHeight, behavior: 'smooth' });
+    });
+
+    afterNavigate(() => {
+        div?.scrollTo({ top: div.scrollHeight, behavior: 'smooth' });
+    });
 </script>
 
 {#if chat}
-	<div class="flex flex-col container h-full mx-auto px-4 md:px-8" style="justify-content: end">
-		<slot name="additional-content-top" />
+    <div class="flex flex-col container h-full mx-auto px-4 md:px-8" style="justify-content: end">
+        <slot name="additional-content-top" />
 
-		<div class="flex flex-col max-w-4xl md:mx-auto space-y-6 pt-6">
-			<!-- Message history -->
-			<ChatMessages {slug} siblings={chat.messages} on:editMessage />
+        <div class="flex flex-col max-w-4xl md:mx-auto space-y-6 pt-6">
+            <ChatMessages {slug} siblings={chat.messages} on:editMessage />
 
-			<!-- Live Message -->
-			{#if $isLoadingAnswerStore}
-				<div class="place-self-start">
-					<div class="p-5 rounded-2xl variant-ghost-tertiary rounded-tl-none">
-						{@html snarkdown($enhancedLiveAnswerStore.content)}
-					</div>
-				</div>
-			{/if}
-		</div>
+            {#if $isLoadingAnswerStore}
+                <div class="place-self-start">
+                    <div class="p-5 rounded-2xl variant-ghost-tertiary rounded-tl-none">
+                        {@html snarkdown($enhancedLiveAnswerStore.content)}
+                    </div>
+                </div>
+            {/if}
+        </div>
 
-		<slot name="additional-content-bottom" />
+        <slot name="additional-content-bottom" />
 
-		<!-- Progress indicator -->
-		<div
-			class="animate-pulse md:w-12 self-center py-2 md:py-6"
-			class:invisible={!$isLoadingAnswerStore}
-		>
-			<ProgressRadial
-				class="w-8"
-				stroke={120}
-				meter="stroke-tertiary-500"
-				track="stroke-tertiary-500/30"
-			/>
-		</div>
-	</div>
+        <div class="animate-pulse md:w-12 self-center py-2 md:py-6" class:invisible={!$isLoadingAnswerStore}>
+            <ProgressRadial class="w-8" stroke={120} meter="stroke-tertiary-500" track="stroke-tertiary-500/30" />
+        </div>
+
+        {#if !isTimeUp}
+            <!-- Chat input component here -->
+        {:else}
+            <div class="chat-disabled-message">
+                The chat session has ended. Thank you for participating.
+            </div>
+        {/if}
+    </div>
 {/if}
